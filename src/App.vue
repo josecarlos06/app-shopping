4a276bbbbb8145470c53da43a520b9af251728e5
josecarlos06 <l181130425@gamadero.tecnm.mx>
2022-08-18 00:46:46 +0000

    ocultar y mostrat

--- src/App.vue
+++ src/App.vue
@@ -1,47 +1,74 @@
 <script setup>
-import HelloWorld from './components/HelloWorld.vue'
-import TheWelcome from './components/TheWelcome.vue'
+// creando una referencia reactiva
+import {ref} from 'vue';
+
+const header = ref("Lista de cursos");
+const editing = ref(false);
+const items = ref([
+    {id : 1, label: 'Typescript'},
+    {id : 2, label: 'Carbon'},
+    {id : 3, label: 'Javascript'},
+]);
+
+const newItem =ref('');
+const newItemPriority = ref(false);
+
+// metodos
+const saveItem = () => {
+    items.value.push({id : items.value.length+1,label : newItem.value })
+};
+const toEdit = (edit) => {
+    editing.value = edit
+}
+
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" />
+<div class="tareas container">
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="task__contenedor">
+          <h2 class="heading">{{header}}</h2>
+          <!-- boton cancelar -->
+          <button class="bton cancelar" v-if="editing" @click="toEdit(false)">
+            Cancelar
+          </button>
+          <!-- botn agregar -->
+          <button v-else class="bton aceptar" @click="toEdit(true)">
+            Agregar Tarea
+          </button>
     </div>
-  </header>
 
-  <main>
-    <TheWelcome />
-  </main>
+    <!-- container form -->
+    <div class="form" v-if="editing">
+        <div class="campo">
+            <input 
+            @keyup.enter="saveItem"
+            type="text" 
+            v-model="newItem"
+            >
+
+            <button 
+            class="bton campo__agrega" 
+            @click="saveItem">
+              Agregar
+            </button>
+        </div>
+        <div class="prioridad">
+            <strong class="strong">Nivel de prioridad</strong>
+            <input type="checkbox" v-model="newItemPriority"/>
+        </div>
+    </div>
+    
+    <!-- condicional -->
+    <p v-if="items.length === 0" class="no-hay">No hay tareas</p>
+
+    <!-- renderizar listas -->
+    <ul>
+        <li v-for="{label, id} in items" :key="id">
+            <p class="item__parrafo">{{label}}</p>
+        </li>
+    </ul>
+
+</div>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
